86a1b8bbb5553244f957475fe59959a81a08c97b
Sanyam <dakshsanyam19@gmail.com>
2024-10-05 19:09:52 +0530

    Added initial files

---
+++ script.js
@@ -0,0 +1,64 @@
+const chatIp = document.querySelector(".input textarea");
+const sendChat = document.querySelector(".input span");
+const chatBox = document.querySelector(".chatbox");
+const toggler = document.querySelector(".toggler");
+
+let userInput;
+const apiKey = "AIzaSyDcr-0Fx59KZPcg8l9toERZ8gqc6vLuj4A";
+
+const createMessage = (message, className) => {
+  const outMessage = document.createElement("out");
+  outMessage.classList.add("chat", className);
+  let chatContent =
+    className === "outgoing"
+      ? `<p></p>`
+      : `<span class="hello">🤖</span><p>/p>`;
+  outMessage.innerHTML = chatContent;
+  outMessage.querySelector("p").textContent = message;
+  return outMessage;
+};
+
+const generateResponse = (incomingBot) => {
+  const apiUrl = `https://generativelanguage.googleapis.com/v1/models/gemini-pro:generateContent?key=${apiKey}`;
+  const botMessage = incomingBot.querySelector("p");
+  const request = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      contents: [
+        {
+          role: "user",
+          parts: [{ text: userInput }],
+        },
+      ],
+    }),
+  };
+  fetch(apiUrl, request)
+    .then((res) => res.json())
+    .then((data) => {
+      botMessage.textContent = data.candidates[0].content.parts[0].text;
+      //   console.log(data);
+    })
+    .catch((error) => {
+      botMessage.textContent = "Oops! Something went wrong.Please try again";
+    })
+    .finally(() => chatBox.scrollTo(0, chatBox.scrollHeight));
+};
+
+const handleChat = () => {
+  userInput = chatIp.value.trim();
+  if (!userInput) return;
+  chatIp.value = "";
+  chatBox.appendChild(createMessage(userInput, "outgoing"));
+  chatBox.scrollTo(0, chatBox.scrollHeight);
+  setTimeout(() => {
+    const incomingBot = createMessage("Generating...", "incoming");
+    chatBox.appendChild(incomingBot);
+    chatBox.scrollTo(0, chatBox.scrollHeight);
+    generateResponse(incomingBot);
+  }, 600);
+};
+toggler.addEventListener("click", () => document.body.classList.toggle("show"));
+sendChat.addEventListener("click", handleChat);
